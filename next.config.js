20b7452e0a14a4505b51b3e5df877e9e9f3a2eb0
Felix Koschmidder <felix.koschi@gmail.com>
2022-12-19 19:59:11 +0100

    chore(s3): add aws s3 bucket

--- next.config.js
+++ next.config.js
@@ -4,10 +4,12 @@ const store = require("./store.config.json")
 module.exports = withStoreConfig({
   features: store.features,
   reactStrictMode: true,
+  output: 'standalone',
   images: {
     domains: [
       "medusa-public-images.s3.eu-west-1.amazonaws.com",
       "http://127.0.0.1:9001",
+      "salty-caramel.s3.amazonaws.com",
       "localhost"
     ],
   },
